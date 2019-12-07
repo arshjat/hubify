ace2076c82e93dee88de0a12e456714b9fea9999
Sujeet Gautam <success@Sujeets-MacBook-Pro.local>
2019-12-07 20:18:33 +0530

    changes in apollo.js

--- apollo.js
+++ apollo.js
@@ -1,19 +1,34 @@
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-
-const GRAPHQL_ENDPOINT = `https://proto-app-backen.herokuapp.com/v1alpha1/graphql`;
+import { GRAPHQL_ENDPOINT } from './config/Hasura';
+import { ApolloLink, concat } from 'apollo-link';
 
 const createApolloClient = (token) => {
-  const link = new HttpLink({
+  const httpLink = new HttpLink({
     uri: GRAPHQL_ENDPOINT,
     headers: {
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
     }
   });
+
+  
+  const defaultOptions = {
+		watchQuery: {
+			fetchPolicy: 'no-cache',
+			errorPolicy: 'ignore',
+		},
+		query: {
+			fetchPolicy: 'no-cache',
+			errorPolicy: 'all',
+		},
+	};
+
   return new ApolloClient({
     link,
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    defaultOptions: defaultOptions
   })
 }
 
