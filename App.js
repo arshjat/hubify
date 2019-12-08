d8db70ab7bea18bd8fad8795a14d7a1f4e4e224b
Sujeet Gautam <success@Sujeets-MacBook-Pro.local>
2019-12-08 23:30:52 +0530

    removed unnessary components

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-// import { Ionicons } from '@expo/vector-icons';
-
 
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
